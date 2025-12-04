90b213ae25558a2d44c69af27db9ea8f8477878f
Kevin Dalli <99217852+bulutthecat@users.noreply.github.com>
2025-12-03 21:29:05 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Start Xephyr
-Xephyr -br -ac -noreset -screen 800x600 -mouse ephyr,,,5 :7 &
-xephyr_pid=$!
-sleep 1
+#Xephyr -br -ac -noreset -screen 800x600 -mouse ephyr,,,5 :7 &
+#xephyr_pid=$!
+#sleep 1
 
-export DISPLAY=:7
+#export DISPLAY=:7
 
 # SET BACKGROUND TO GREY so we can see black windows
 xsetroot -solid gray
@@ -15,12 +15,12 @@ python3 main.py &
 wm_pid=$!
 
 # Run xterm
-sleep 1
-xterm &
-xterm &
-xclock &
-xcalc &
-xeyes &
+#sleep 1
+#xterm &
+#xterm &
+#xclock &
+#xcalc &
+#xeyes &
 
 wait $wm_pid
-kill $xephyr_pid
+#kill $xephyr_pid
